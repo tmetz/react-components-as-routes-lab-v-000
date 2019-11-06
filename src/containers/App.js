219ca9c0dbcd1d4f55812be1554b38aa550009d8
Tammy Metz <tammy@tammymetz.com>
2019-11-06 02:03:47 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ const App = (props) => {
     <Router>
 
       <div>
-        <Navbar />
+        <NavBar />
         <Route exact path = "/" component={Home} />
         <Route exact path = "/movies" component ={Movies} />
         <Route exact path = "/directors" component={Directors} />
