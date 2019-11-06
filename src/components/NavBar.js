498a43031044bc25f42d6170b8a1363aa26d66fd
Tammy Metz <tammy@tammymetz.com>
2019-11-06 01:51:23 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Moview</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </div>
   );
 };
